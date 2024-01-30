896eb07e2e1a465ae02b254477e63369baa2e958
Ishkhan <ioterzian@gmail.com>
2024-01-30 11:19:38 -0500

    Completed RESTful implementation of Game & Pricing Objects

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from flask import Flask, jsonify, request
 from models import Developer, Genre, Platform, Game, Pricing
 from db import create_engine_and_session
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
@@ -76,6 +77,12 @@ def add_developer():
     - JSON: Success message or an error if unsuccessful.
     """
     name = request.args.get('name')
+
+    developer = session.query(Developer).filter(Developer.name == name).scalar()
+
+    if developer:
+        return jsonify(error="Developer already exists."), 404
+
     new_developer = Developer(name=name)
     session.add(new_developer)
     session.commit()
@@ -95,8 +102,7 @@ def update_developer(developer_id):
     - JSON: Success message or an error if the developer is not found.
     """
     new_name = request.args.get('name')
-    results = session.query(Developer).filter(Developer.id == developer_id)
-    developer = results.scalar()
+    developer = session.query(Developer).filter(Developer.id == developer_id).scalar()
 
     if not developer:
         return jsonify(error="Developer not found"), 404
@@ -118,8 +124,7 @@ def delete_developer(developer_id):
     Returns:
     - JSON: Success message or an error if the developer is not found.
     """
-    results = session.query(Developer).filter(Developer.id == developer_id)
-    developer = results.scalar()
+    developer = session.query(Developer).filter(Developer.id == developer_id).scalar()
 
     if not developer:
         return jsonify(error="Developer not found"), 404
@@ -189,7 +194,14 @@ def add_genre():
     Returns:
     - JSON: Success message or an error if unsuccessful.
     """
+
     name = request.args.get('name')
+
+    genre = session.query(Genre).filter(Genre.name == name).scalar()
+
+    if genre:
+        return jsonify(message="Genre already exists."), 404
+
     new_genre = Genre(name=name)
     session.add(new_genre)
     session.commit()
@@ -209,8 +221,7 @@ def update_genre(genre_id):
     - JSON: Success message or an error if the genre is not found.
     """
     new_name = request.args.get('name')
-    results = session.query(Genre).filter(Genre.id == genre_id)
-    genre = results.scalar()
+    genre = session.query(Genre).filter(Genre.id == genre_id).scalar()
 
     if not genre:
         return jsonify(error="Genre not found"), 404
@@ -232,8 +243,7 @@ def delete_genre(genre_id):
     Returns:
     - JSON: Success message or an error if the genre is not found.
     """
-    results = session.query(Genre).filter(Genre.id == genre_id)
-    genre = results.scalar()
+    genre = session.query(Genre).filter(Genre.id == genre_id).scalar()
 
     if not genre:
         return jsonify(error="Genre not found"), 404
@@ -259,11 +269,9 @@ def get_all_platform():
     platforms = []
     results = session.query(Platform).all()
     for platform in results:
-        platform_id = platform.id
-        name = platform.console_name
         platforms.append({
-            "id": platform_id,
-            "name": name
+            "id": platform.id,
+            "name": platform.name
         })
     return jsonify(platforms=platforms), 200
 
@@ -287,7 +295,7 @@ def get_single_platform(platform_id):
 
     found_platform = [{
         "id": platform.id,
-        "name": platform.console_name
+        "name": platform.name
     }]
 
     return jsonify(platform=found_platform), 200
@@ -303,7 +311,13 @@ def add_platform():
     - JSON: Success message or an error if unsuccessful.
     """
     name = request.args.get('name')
-    new_platform = Platform(console_name=name)
+
+    platform = session.query(Platform).filter(Platform.name == name).scalar()
+
+    if platform:
+        return jsonify(message="Platform already exists."), 404
+
+    new_platform = Platform(name=name)
     session.add(new_platform)
     session.commit()
     return jsonify(message="Successfully added the new Platform"), 200
@@ -322,13 +336,12 @@ def update_platform(platform_id):
     - JSON: Success message or an error if the genre is not found.
     """
     new_name = request.args.get('name')
-    results = session.query(Platform).filter(Platform.id == platform_id)
-    platform = results.scalar()
+    platform = session.query(Platform).filter(Platform.id == platform_id).scalar()
 
     if not platform:
         return jsonify(error="Platform not found"), 404
 
-    platform.console_name = new_name
+    platform.name = new_name
     session.commit()
     return jsonify(message="Successfully Updated the Platform"), 200
 
@@ -345,8 +358,7 @@ def delete_platform(platform_id):
     Returns:
     - JSON: Success message or an error if the platform is not found.
     """
-    results = session.query(Platform).filter(Platform.id == platform_id)
-    platform = results.scalar()
+    platform = session.query(Platform).filter(Platform.id == platform_id).scalar()
 
     if not platform:
         return jsonify(error="Platform not found"), 404
@@ -355,4 +367,235 @@ def delete_platform(platform_id):
     session.commit()
     return jsonify(message="Successfully Deleted the Platform"), 200
 
+
 # ============================ PLATFORM END =======================================
+
+# ============================ GAME START =======================================
+
+# Get all Games
+@app.route('/api/game')
+def get_all_games():
+    games = []
+    results = session.query(Game).order_by(Game.id).all()
+
+    for game in results:
+        developer_name = session.query(Developer.name).filter(Developer.id == game.developer_id).scalar()
+        genre = session.query(Genre.name).filter(Genre.id == game.genre_id).scalar()
+        platform = session.query(Platform.name).filter(Platform.id == game.platform_id).scalar()
+
+        games.append({
+            "id": game.id,
+            "title": game.title,
+            "release_date": game.release_date,
+            "description": game.description,
+            "gameplay_modes": game.gameplay_modes,
+            "img_url": game.img_url,
+            "developer": developer_name,
+            "genre": genre,
+            "platform_id": platform
+        })
+
+    return jsonify(games=games), 200
+
+
+# Get a single Game
+@app.route('/api/game/<int:game_id>', methods=['GET'])
+def get_single_game(game_id):
+    game = []
+    found_game = session.query(Game).filter(Game.id == game_id).scalar()
+
+    if not found_game:
+        return jsonify(error="Game not found"), 404
+
+    developer_name = session.query(Developer.name).filter(Developer.id == found_game.id).scalar()
+    genre = session.query(Genre.name).filter(Genre.id == found_game.genre_id).scalar()
+    platform = session.query(Platform.name).filter(Platform.id == found_game.platform_id).scalar()
+
+    game.append({
+        "id": found_game.id,
+        "title": found_game.title,
+        "release_date": found_game.release_date,
+        "description": found_game.description,
+        "gameplay_modes": found_game.gameplay_modes,
+        "img_url": found_game.img_url,
+        "developer": developer_name,
+        "genre": genre,
+        "platform_id": platform
+    })
+
+    return jsonify(game=game), 200
+
+
+# Add a Game
+@app.route('/api/game', methods=['POST'])
+def add_game():
+    title = request.args.get('title')
+    description = request.args.get('description')
+    release_date_str = request.args.get('release_date')
+    gameplay_modes = request.args.get('gameplay_modes')
+    img_url = request.args.get('img_url')
+    developer_name = request.args.get('developer')
+    genre_name = request.args.get('genre')
+    platform_name = request.args.get('platform')
+
+    platform = session.query(Platform).filter(Platform.name == platform_name).scalar()
+
+    if not platform:
+        return jsonify(error="Platform does not exist, please add the Platform to the database"), 404
+
+    existing_game = session.query(Game).filter(Game.title == title, Game.platform_id == platform.id).scalar()
+
+    if existing_game:
+        return jsonify(error="Game already exists for this platform."), 409
+
+    developer = session.query(Developer).filter(Developer.name == developer_name).scalar()
+
+    if not developer:
+        return jsonify(error="Developer does not exist, please add the developer to the database"), 404
+
+    genre = session.query(Genre).filter(Genre.name == genre_name).scalar()
+
+    if not genre:
+        return jsonify(error="Genre does not exist, please add the Genre to the database"), 404
+
+    release_date = datetime.strptime(release_date_str, '%B/%d/%Y').date()
+
+    new_game = Game(title=title, description=description, release_date=release_date, gameplay_modes=gameplay_modes,
+                    img_url=img_url, developer_id=developer.id, genre_id=genre.id, platform_id=platform.id)
+
+    session.add(new_game)
+    session.commit()
+
+    return jsonify(message="Successfully added the Game"), 200
+
+
+# Update a Game
+@app.route('/api/game/<int:game_id>', methods=['PATCH'])
+def update_game(game_id):
+    game = session.query(Game).filter(Game.id == game_id).scalar()
+
+    if not game:
+        return jsonify(error="Game does not exist"), 404
+
+    title = request.args.get('title')
+    description = request.args.get('description')
+    gameplay_modes = request.args.get('gameplay_modes')
+    img_url = request.args.get('img_url')
+
+    game.title = title
+    game.description = description
+    game.gameplay_modes = gameplay_modes
+    game.img_url = img_url
+
+    session.commit()
+
+
+# Delete a Game
+@app.route('/api/game/<int:game_id>', methods=['DELETE'])
+def delete_game(game_id):
+    game = session.query(Game).filter(Game.id == game_id).scalar()
+
+    if not game:
+        return jsonify(error="Game does not exist"), 404
+
+    session.delete(game)
+    session.commit()
+    return jsonify(message="Successfully delete the Game"), 200
+
+
+# ============================ GAME END =======================================
+
+
+# ============================ PRICING START =======================================
+
+# Getting all Pricing information
+@app.route('/api/pricing')
+def get_all_pricing():
+    all_prices = []
+    results = session.query(Pricing).all()
+
+    for pricing in results:
+        game = session.query(Game).filter(Game.id == pricing.game_id).first()
+
+        if game:
+            all_prices.append({
+                "title": game.title,
+                "year": pricing.year,
+                "price": pricing.price
+            })
+
+    return jsonify(pricings=all_prices), 200
+
+
+# Get Pricing for a single Game
+@app.route('/api/pricing/<int:game_id>', methods=['GET'])
+def get_single_game_pricing(game_id):
+    year = request.args.get('year')
+    pricing = session.query(Pricing).filter_by(game_id=game_id, year=year).first()
+
+    if not pricing:
+        return jsonify(error="Pricing information not found for the specified game and year"), 404
+
+    pricing_info = [{
+        "game_id": pricing.game_id,
+        "year": pricing.year,
+        "price": pricing.price
+    }]
+
+    return jsonify(pricing=pricing_info), 200
+
+
+# Add Pricing for a Game
+@app.route('/api/pricing/<int:game_id>', methods=['POST'])
+def add_game_pricing(game_id):
+    price = request.args.get('price')
+    year = request.args.get('year')
+
+    existing_pricing = session.query(Pricing).filter_by(game_id=game_id, year=year).first()
+
+    if existing_pricing:
+        return jsonify(error="Pricing information already exists for the specified game and year"), 409
+
+    game_exists = session.query(Game).filter(Game.id == game_id).scalar()
+
+    if not game_exists:
+        return jsonify(error="Game not found"), 404
+
+    pricing = Pricing(game_id=game_id, year=year, price=price)
+    session.add(pricing)
+    session.commit()
+
+    return jsonify(message="Successfully added pricing information for the Game"), 200
+
+
+# Update Pricing for a Game
+@app.route('/api/pricing/<int:game_id>', methods=['PATCH'])
+def update_game_pricing_or_year(game_id):
+    new_price = request.args.get('price')
+    new_year = request.args.get('year')
+
+    pricing = session.query(Pricing).filter_by(game_id=game_id, year=new_year).first()
+
+    if not pricing:
+        return jsonify(error="Pricing information not found for the specified game and year"), 404
+
+    pricing.price = new_price
+    pricing.year = new_year
+    session.commit()
+
+    return jsonify(message="Successfully updated pricing information for the Game"), 200
+
+# Delete Pricing for a Game
+@app.route('/api/pricing/<int:game_id>', methods=['DELETE'])
+def delete_game_pricing(game_id):
+    pricing = session.query(Pricing).filter(Pricing.game_id == game_id).scalar()
+
+    if not pricing:
+        return jsonify(error="Pricing information not found for the specified game and year"), 404
+
+    session.delete(pricing)
+    session.commit()
+
+    return jsonify(message="Successfully deleted pricing information for the Game"), 200
+
+# ============================ PRICING END =======================================
--- models.py
+++ models.py
@@ -8,7 +8,7 @@ class Developer(Base):
     """Model for Developers table."""
     __tablename__ = "developers"
     id = Column("id", Integer, primary_key=True)
-    name = Column("name", String, nullable=False)
+    name = Column("name", String, nullable=False, unique=True)
 
     def __repr__(self):
         return f"Developer_id: {self.id} Developer_name: {self.name}"
@@ -18,7 +18,7 @@ class Genre(Base):
     """Model for Genres table."""
     __tablename__ = "genres"
     id = Column("id", Integer, primary_key=True)
-    name = Column("name", String, nullable=False)
+    name = Column("name", String, nullable=False, unique=True)
 
     def __repr__(self):
         return f"Genre_id: {self.id} Genre_name: {self.name}"
@@ -28,10 +28,10 @@ class Platform(Base):
     """Model for Platforms table."""
     __tablename__ = "platforms"
     id = Column("id", Integer, primary_key=True)
-    console_name = Column("console_name", String, nullable=False)
+    name = Column("name", String, nullable=False, unique=True)
 
     def __repr__(self):
-        return f"Platform_id: {self.id} Console_name: {self.console_name}"
+        return f"Platform_id: {self.id} name: {self.name}"
 
 
 class Game(Base):
@@ -39,14 +39,14 @@ class Game(Base):
     __tablename__ = "games"
     id = Column("id", Integer, primary_key=True)
     title = Column("title", String, nullable=False)
-    release_date = Column("release_date", Date, server_default=func.now(), onupdate=func.now())
-    description = Column("description", String)
-    gameplay_modes = Column("gameplay_modes", String)
+    release_date = Column("release_date", Date, server_default=func.now(), onupdate=func.now(), nullable=False)
+    description = Column("description", String, nullable=False)
+    gameplay_modes = Column("gameplay_modes", String, nullable=False)
     img_url = Column("img_url", String)
 
-    developer_id = Column("developer_id", Integer, ForeignKey("developers.id", ondelete="CASCADE"), index=True)
-    genre_id = Column("genre_id", Integer, ForeignKey("genres.id", ondelete="CASCADE"), index=True)
-    platform_id = Column("platform_id", Integer, ForeignKey("platforms.id", ondelete="CASCADE"), index=True)
+    developer_id = Column("developer_id", Integer, ForeignKey("developers.id", ondelete="CASCADE"), index=True, nullable=False)
+    genre_id = Column("genre_id", Integer, ForeignKey("genres.id", ondelete="CASCADE"), index=True, nullable=False)
+    platform_id = Column("platform_id", Integer, ForeignKey("platforms.id", ondelete="CASCADE"), index=True, nullable=False)
 
     def __repr__(self):
         return f"Game_id: {self.id} Title: {self.title} Release_date: {self.release_date}"
